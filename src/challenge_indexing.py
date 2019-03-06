4890fd649513cbd4d0ec65ae9e5f95a1b3e8020f
maxcan7 <maxcan7@gmail.com>
2019-03-06 15:53:30 -0500

    reorganized code for clarity, outlined how TFIDF will be calculated

--- src/challenge_indexing.py
+++ src/challenge_indexing.py
@@ -1,6 +1,5 @@
 #! /usr/bin/env python
 
-
 # For reading data directory
 import os
 import sys
@@ -39,68 +38,76 @@ def context(config):
     return sc, sqlContext
 
 
-# Indexing function which tokenizes terms and computes TF
-def indexing_fun(**kwargs):
-    # Load in each book and make each line a document
-    book = sc.wholeTextFiles(file).map(lambda x: x[1])
-    # Run the preprocess function on the book and zip
-    book = book.mapPartitions(preproc).zipWithIndex()
-    # Transform term tokens RDD to dataframe
-    book = sqlContext.createDataFrame(book, ["list_of_words", "index"])
-    # Calculate TF from raw count of terms
-    cv = CountVectorizer(inputCol="list_of_words",
-                         outputCol="raw_features")
-    # Create count vector model given parameters
-    cvmodel = cv.fit(book)
-    # Add raw count to book dataframe
-    book = cvmodel.transform(book)
-    # Replace index with book ID
-    book = book.withColumn("index", when(book["index"] == 0, doc))
-    # Create new table reformatted where each row is a unique word, with column for word, book index, and raw count
-    TF = book.select("index", explode("list_of_words").alias("words")).groupBy("index", "words").count()
-    # Get sum of all words
-    N = TF.groupBy().sum().collect()[0][0]
-    # take square root of term count / total number of terms for TF
-    termfreq = TF.select("count").rdd.map(lambda count: [(x/N)**(1/2.0) for x in count]).toDF()
-    termfreq = termfreq.withColumnRenamed("_1", "TF")
-    # Obnoxious hoops to add term frequency ("TF") to TF dataframe
-    TF = TF.withColumn("id", monotonically_increasing_id())
-    termfreq = termfreq.withColumn("id", monotonically_increasing_id())
-    TF = TF.join(termfreq, "id", "outer").drop("id")
-    # Write outputs
-    book.select('list_of_words', 'index', 'raw_features').write.save('countvec_'+doc+'.parquet')
-    TF.select('index', 'words', 'count', 'TF').write.save('TF_'+doc+'.parquet')
+# Calculates raw count and term frequency for each term in each book,
+# writes each book separately
+def TF_fun():
+    # Load in each file and calculate TF
+    for filename in os.listdir(os.getcwd()):
+        file = datapath + filename
+        # Load in each book and make each line a document
+        book = sc.wholeTextFiles(file).map(lambda x: x[1])
+        # Run the preprocess function on the book and zip
+        book = book.mapPartitions(preproc).zipWithIndex()
+        # Transform term tokens RDD to dataframe
+        book = sqlContext.createDataFrame(book, ["list_of_words", "index"])
+        # Calculate TF from raw count of terms
+        cv = CountVectorizer(inputCol="list_of_words",
+                             outputCol="raw_features")
+        # Create count vector model given parameters
+        cvmodel = cv.fit(book)
+        # Add raw count to book dataframe
+        book = cvmodel.transform(book)
+        # Replace index with book ID
+        book = book.withColumn("index", when(book["index"] == 0, filename))
+        # Create new table reformatted where each row is a unique word, with column for word, book index, and raw count
+        TF = book.select("index", explode("list_of_words").alias("words")).groupBy("index", "words").count()
+        # Get sum of all words
+        N = TF.groupBy().sum().collect()[0][0]
+        # take square root of term count / total number of terms for TF
+        termfreq = TF.select("count").rdd.map(lambda count: [(x/N)**(1/2.0) for x in count]).toDF()
+        termfreq = termfreq.withColumnRenamed("_1", "TF")
+        # Obnoxious hoops to add term frequency ("TF") to TF dataframe
+        TF = TF.withColumn("id", monotonically_increasing_id())
+        termfreq = termfreq.withColumn("id", monotonically_increasing_id())
+        TF = TF.join(termfreq, "id", "outer").drop("id")
+        # Write outputs
+        book.select('list_of_words', 'index', 'raw_features').write.save('countvec_'+filename+'.parquet')
+        TF.select('index', 'words', 'count', 'TF').write.save('TF_'+filename+'.parquet')
 
 
-# Calculate TF-IDF from full corpus
-def tfidf_fun():
+# Get running docCount, or number of docs a given term appears in
+def docCount_fun():
     # Load each file and do a running count of all terms for calculating TFIDF
-    totCounts = []
+    docCounts = []
     for filename in os.listdir(os.getcwd()):
         file = datapath + filename
         TF = spark.read.load(mainpath+'TF'+filename+'.parquet')
-        if not totCounts:
+        if not docCounts:
             # Add a column for words where each row is a unique word
-            totCounts = TF.select('words')
+            docCounts = TF.select('words')
             # Add a column for running count of number of documents each word appears in
-            totCounts = totCounts.withColumn('docCount', lit(1))
+            docCounts = docCounts.withColumn('docCount', lit(1))
         else:
-            # Find words present in totCounts and current book and update count
-            updatewords = totCounts.select('words').intersect(TF.select('words'))
-            tmp = totCounts.withColumn('docCount', when(totCounts.words.alias('old') == updatewords.words.alias('new'), totCounts.docCount + 1).otherwise(totCounts.docCount))
-            tmp2 = totCounts.select('words', (totCounts.words.alias('old') == updatewords.words.alias('new')).alias('update'))
-            # Subtract from TF words already present in totCounts
-            newwords = TF.select('words').subtract(totCounts.select('words')).withColumn('docCount', lit(1))
-            # Add new words to totCounts
-            totCounts = totCounts.unionAll(newwords)
+            # Find words present in docCounts and current book and update count
+            # THIS PART CURRENTLY DOES NOT WORK
+            updatewords = docCounts.select('words').intersect(TF.select('words'))
+            tmp = docCounts.withColumn('docCount', when(docCounts.words.alias('old') == updatewords.words.alias('new'), docCounts.docCount + 1).otherwise(docCounts.docCount))
+            tmp2 = docCounts.select('words', (docCounts.words.alias('old') == updatewords.words.alias('new')).alias('update'))
+            # Subtract from TF words already present in docCounts
+            newwords = TF.select('words').subtract(docCounts.select('words')).withColumn('docCount', lit(1))
+            # Add new words to docCounts
+            docCounts = docCounts.unionAll(newwords)
+    docCounts.select('words', 'docCount').write.save('doc_count.parquet')
 
 
-
-    # IDF
-    idf = IDF(inputCol="raw_features",
-              outputCol="features")
-    idfModel = idf.fit(book)
-    tfidf_vecs = idfModel.transform(book)
+# Iteratively calculate TFIDF given TF and docCounts
+def TFIDF_fun():
+    # Load total docCounts
+    docCounts = spark.read.load('doc_count.parquet')
+    # For each book, load in TF dataframe and calculate TFIDF from TF and docCounts
+    for filename in os.listdir(os.getcwd()):
+        file = datapath + filename
+        TF = spark.read.load(mainpath+'TF'+filename+'.parquet')
 
 
 # Iterator function for processing partitioned data
@@ -137,7 +144,12 @@ config = {
 # Run pipeline functions
 if __name__ == '__main__':
     [sc, sqlContext] = context(config)
-    for filename in os.listdir(os.getcwd()):
-        file = datapath + filename
-        indexing_fun(file=file, sc=sc, sqlContext=sqlContext, doc=filename)
-    tfidx_vecs = tfidf_fun()
+
+    # Calculate and write TF for each doc
+    TF_fun()
+
+    # Calculate and write docCounts across all docs
+    docCount_fun()
+
+    # Calculate and write TFIDF from TF and docCounts
+    TFIDF_fun()
